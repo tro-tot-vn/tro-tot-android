62e2e46d63e7898c4819e2f674065f1d2fe16c4f
lamdx4 <dagxuanlam@gmail.com>
2026-03-06 13:58:37 +0700

    feat(room): add dependencies and init class Database

---
+++ app/src/main/java/com/trototvn/trototandroid/data/local/AppDatabase.java
@@ -0,0 +1,8 @@
+package com.trototvn.trototandroid.data.local;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+}
